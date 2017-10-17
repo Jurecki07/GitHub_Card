c3aad2776e8011a4197b20728f2a63570573ca03
Tyler Jurecki <jurecki07@yahoo.com>
2017-10-17 09:26:57 -0500

    tanner working with me

--- main.js
+++ main.js
@@ -3,14 +3,13 @@ let info = document.querySelector(".info");
 let bio = document.querySelector(".bio");
 let profile = document.querySelector(".img");
 let title = document.querySelector(".title");
-var request = require('ajax-request');
 
-// Ajax Request To My GitHub
-let req = new XMLHttpRequest();
-req.open("GET", "https://api.github.com/users/Jurecki07");
-req.addEventListener("load", reqListener);
-req.send();
 
-function reqListener() {
-    let data = JSON.parse(this.responseText);
+
+// function that retrieves github data. 
+function getGitHubData() {
+    //  "https://api.github.com/users/Jurecki07"
+    // fetch or axios request using this url
+    // populate them html with the correct data from github
+    
 }
